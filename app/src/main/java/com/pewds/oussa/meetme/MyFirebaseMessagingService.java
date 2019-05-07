7e5933a5c9deb08f85a5fbb5c3b9909267a0fb33
OussEmaDevCode <oussamahassini6@gmail.com>
2019-05-07 11:00:02 +0100

    added FCM

---
+++ app/src/main/java/com/pewds/oussa/meetme/MyFirebaseMessagingService.java
@@ -0,0 +1,51 @@
+package com.pewds.oussa.meetme;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        Notify();
+    }
+    private void Notify(){
+        Intent intent = new Intent(this, Principal.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Bitmap b = BitmapFactory.decodeResource(getResources(),R.mipmap.meetme);
+        Uri sound = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.notify);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Meet Me")
+                .setLargeIcon(b)
+                .setSmallIcon(R.drawable.ic_speech_bubble)
+                .setContentTitle("You got messages")
+                .setContentText("Don't forget to check your messages")
+                .setSound(sound)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "messages";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("Meet Me", name, importance);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, builder.build());
+    }
+}
